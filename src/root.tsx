24555bf4a3e15811b9fbbecd9f97588d8398add9
Shai Reznik <shairez@users.noreply.github.com>
2022-08-04 18:00:28 +0300

    added social image

--- src/root.tsx
+++ src/root.tsx
@@ -8,6 +8,20 @@ export default () => {
       <head>
         <meta charSet="utf-8" />
         <title>Qwik Community</title>
+        <meta property="og:url" content="https://www.qwikcommunity.com" />
+        <meta property="og:title" content="Qwik Community" />
+        <meta property="og:description" content="Check out all the Qwik communities around the world" />
+        <meta property="og:image" content="qwik-community.jpg" />
+        <meta property="og:image:alt" content="Qwik Community" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Qwik Community" />
+        <meta property="og:locale" content="en_US" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Qwik Community" />
+        <meta name="twitter:description" content="Check out all the Qwik communities around the world" />
+        <meta name="twitter:image" content="qwik-community.jpg" />
+        <meta name="twitter:image:alt" content="Qwik Community" />
       </head>
       <body>
         <App />
